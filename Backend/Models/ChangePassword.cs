5c9a9b9943a346ad4b6f93f7bb0068343b9c863f
Jaikiran976 <142710462+Jaikiran976@users.noreply.github.com>
2025-05-13 17:48:45 +0530

    Remove folder-name from the repo

---
+++ Backend/Models/ChangePassword.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class ChangePassword
+    {
+        public string usernameOrEmail { get; set; }
+
+        public string securityQuestion { get; set; }
+
+        public string securityAnswer { get; set; }
+
+        public string newPassword { get; set; }
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,57 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+var builder = WebApplication.CreateBuilder(args);
+var frontendUrl = builder.Configuration.GetValue<string>("FrontendSettings:FrontendUrl"); 
+
+if (string.IsNullOrEmpty(frontendUrl))
+{
+    // Handle the case where frontendUrl is missing
+    frontendUrl = "";  // Set a fallback URL
+}
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure MySQL
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+//Configure mongo db
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+builder.Services.AddSingleton<AppDbContext>();
+
+var app = builder.Build();
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    //dbContext.Database.Migrate();  // Automatically applies migrations when the app starts
+//}
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(frontendUrl));
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
