bfe82f30d79976d27f358ee39d63f51fc50666f4
Jaikiran976 <142710462+Jaikiran976@users.noreply.github.com>
2025-06-25 13:11:17 +0530

    pushing the build

---
+++ Frontend/dist/frontend/browser/assets/data/constants/texts.ts
@@ -0,0 +1,59 @@
+
+export const AppText = {
+    hero:{
+        tagline: 'Collect the days that make you, you.',
+        subheading: 'Your personal diary to write, reflect, and remember — one day at a time.'
+    },
+    
+    form: {
+        labels:{
+            usename: 'User Name :',
+            email: 'E-mail :',
+            securityQuestion: 'Journal Recovery Prompt :',
+            securityAnswer: 'Your answer :',
+            password: 'Password :',
+            usernameOrEmail : 'Username or Email :',
+        },
+        placeholders: {
+            usename: 'Enter the Username...',
+            email: 'Enter the Email...',
+            securityQuestion: 'Choose a prompt you\'ll remember',
+            securityAnswer: 'Enter something memorable...',
+            password: 'Enter the Password...',
+            usernameOrEmail : 'Enter a Username or Email...',
+        },
+        errors: {
+            required: 'This field is required.',
+            emailPattern: 'Please enter a valid email address.',
+            minLength: (min: number) => `Please enter at least ${min} characters.`,
+            maxLength: (max: number) => `Must be less than ${max} characters.`,
+            passwordComplexity: "Password must be at least 6 characters and include a lowercase letter, an uppercase letter, a number, and a special character."
+        },
+        buttons: {
+            signup: 'Create Account',
+            signIn: 'Sign In',
+            signInLoading: 'Signing in'
+        }
+    },
+
+    links:{
+        
+    },
+
+    home: {
+        welcome: 'Hello, Alex 👋',
+        startWriting: 'What’s on your mind?',
+        recentEntries: 'Recent Entries',
+        emptyState: 'You haven’t written anything yet. Start your first entry!'
+    },
+
+    settings: {
+        themeToggle: 'Toggle Theme',
+        exportData: 'Export or Backup Your Entries',
+        avatarPicker: 'Choose Your Avatar'
+    },
+
+    textEditor:{
+        placeholder: 'Enter the text...'
+    }
+}
